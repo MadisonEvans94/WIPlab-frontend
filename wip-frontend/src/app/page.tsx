93c6c1b4e8c752535fd495a9fd219f0ba15dc1bb
Madison Evans <madison.evans@postfit.co>
2024-03-28 21:56:08 -0700

    added genre tags

--- wip-frontend/src/app/page.tsx
+++ wip-frontend/src/app/page.tsx
@@ -2,32 +2,119 @@ import CustomPlayer from "@/components/CustomPlayer";
 import { Suspense } from "react";
 
 // https://www.npmjs.com/package/react-player
+// [ ]  fix so that only one song plays at a time
 const Loader = () => <div>Loading player...</div>;
 export default function Home() {
 	return (
 		<div className="px-20">
 			<Suspense fallback={<Loader />}>
-				<CustomPlayer
-					trackMetaData={{
-						trackTitle: "Old Souls",
-						artistName: "Blue Cloud",
-						date: "3-27-2024",
-						genres: ["bounce", "house"],
-					}}
-					url="/testTrack.mp3"
-					comments={[
-						{
-							id: "comment1",
-							time: 10,
-							imageSrc: "/testImage.png",
-						},
-						{
-							id: "comment2",
-							time: 30,
-							imageSrc: "/testImage.png",
-						},
-					]}
-				/>
+				<div className="flex flex-col gap-2">
+					<CustomPlayer
+						trackMetaData={{
+							trackTitle: "Old Souls",
+							artistName: "Blue Cloud",
+							date: "3-27-2024",
+							genres: ["bounce", "house"],
+						}}
+						url="/testTrack.mp3"
+						comments={[
+							{
+								id: "comment1",
+								time: 10,
+								imageSrc: "/testImage.png",
+							},
+							{
+								id: "comment2",
+								time: 30,
+								imageSrc: "/testImage.png",
+							},
+						]}
+					/>
+					{/* <CustomPlayer
+						trackMetaData={{
+							trackTitle: "Old Souls",
+							artistName: "Blue Cloud",
+							date: "3-27-2024",
+							genres: ["bounce", "house"],
+						}}
+						url="/testTrack.mp3"
+						comments={[
+							{
+								id: "comment1",
+								time: 10,
+								imageSrc: "/testImage.png",
+							},
+							{
+								id: "comment2",
+								time: 30,
+								imageSrc: "/testImage.png",
+							},
+						]}
+					/>
+					<CustomPlayer
+						trackMetaData={{
+							trackTitle: "Old Souls",
+							artistName: "Blue Cloud",
+							date: "3-27-2024",
+							genres: ["bounce", "house"],
+						}}
+						url="/testTrack.mp3"
+						comments={[
+							{
+								id: "comment1",
+								time: 10,
+								imageSrc: "/testImage.png",
+							},
+							{
+								id: "comment2",
+								time: 30,
+								imageSrc: "/testImage.png",
+							},
+						]}
+					/>
+					<CustomPlayer
+						trackMetaData={{
+							trackTitle: "Old Souls",
+							artistName: "Blue Cloud",
+							date: "3-27-2024",
+							genres: ["bounce", "house"],
+						}}
+						url="/testTrack.mp3"
+						comments={[
+							{
+								id: "comment1",
+								time: 10,
+								imageSrc: "/testImage.png",
+							},
+							{
+								id: "comment2",
+								time: 30,
+								imageSrc: "/testImage.png",
+							},
+						]}
+					/>
+					<CustomPlayer
+						trackMetaData={{
+							trackTitle: "Old Souls",
+							artistName: "Blue Cloud",
+							date: "3-27-2024",
+							genres: ["bounce", "house"],
+						}}
+						url="/testTrack.mp3"
+						comments={[
+							{
+								id: "comment1",
+								time: 10,
+								imageSrc: "/testImage.png",
+							},
+							{
+								id: "comment2",
+								time: 30,
+								imageSrc: "/testImage.png",
+							},
+						]}
+					/> */}
+				</div>
 			</Suspense>
 		</div>
 	);
